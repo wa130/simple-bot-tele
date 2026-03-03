bb911aad5ce1ffb690dd80662f7f7949635585b9
wa130 <nasyuwa648@gmail.com>
2026-03-03 23:38:44 +0700

    membuat koneksi bot

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import 'dotenv/config'
 import TelegramBot from 'node-telegram-bot-api'
 
-const token = process.env.TOKEN
+const token = process.env.TELE_TOKEN
 
+async function startBot() {
+
+    const bot = new TelegramBot(token, { polling: true })
+    
+    bot.onText(/\/start/, (msg) => {
+        bot.sendMessage(msg.chat.id, "hellow brokk")
+    })
+}
+
+startBot()
