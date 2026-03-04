6301dad1e1474b80a5385c80ff238a939be1e441
Nasyuwa <nasyuwa648@gmail.com>
2026-03-05 00:26:55 +0700

    membuat temlate menu sederhana dengnan button dan denga logika editMessageCaption agar saat di klik akan berubah caption teks nya

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import 'dotenv/config'
 import TelegramBot from 'node-telegram-bot-api'
 
+
 const token = process.env.TELE_TOKEN
 
 async function startBot() {
 
     const bot = new TelegramBot(token, { polling: true })
     
-    bot.onText(/\/start/, (msg) => {
-        bot.sendMessage(msg.chat.id, "hellow brokk")
-    })
-
     bot.on('message', (msg) => {
         const text = msg.text
+        const chatId = msg.chat.id
 
         const prefix = '/'
         const isCmd = text.startsWith(prefix)
@@ -23,9 +21,72 @@ async function startBot() {
         if (!isCmd) return
 
         switch (command) {
-            case 'tes':
-                bot.sendMessage(msg.chat.id, 'testing berhasil')
-                break;
+            case 'start': {
+
+                const menuUtama = `
+                Halo Saya adalah bot monitoring dashboard sederhana
+                `
+                bot.sendPhoto(
+                  msg.chat.id,
+                  "https://myprof.pages.dev/assets/img/logo.png",
+                  {
+                    caption: menuUtama,
+                    parse_mode: "HTML",
+                    reply_markup: {
+                      inline_keyboard: [
+                        [{ text: "klik me", callback_data: "me" }],
+                      ],
+                    },
+                  },
+                );
+
+                bot.on('callback_query', (query) => {
+                    
+                    switch (query.data) {
+                      case "me":
+                        {
+                          bot.editMessageCaption(
+                            "halo ini saya Nasyuwa", {
+                            chat_id: query.message.chat.id,
+                            message_id: query.message.message_id,
+                            reply_markup: {
+                                inline_keyboard: [
+                                    [{ text: 'kembali', callback_data: 'kembali'}]
+                                ]
+                            }
+                            }
+                          );
+                        }
+                        break;
+
+                        case 'kembali': {
+                            bot.editMessageCaption(menuUtama, {
+                              chat_id: query.message.chat.id,
+                              message_id: query.message.message_id,
+                              reply_markup: {
+                                inline_keyboard: [
+                                  [
+                                    {
+                                      text: "klik me",
+                                      callback_data: "me",
+                                    },
+                                  ],
+                                ],
+                              },
+                            });
+                        }
+                        break
+
+                      default:
+                        break;
+                    }
+                })
+            }
+            break
+            case 'tes': {
+                bot.sendMessage(chatId, 'testing berhasil')
+            }
+            break;
         
             default:
                 break;
