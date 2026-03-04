4196d63cc8bf32a927da3458ee2ed72fc8e92505
Nasyuwa <nasyuwa648@gmail.com>
2026-03-04 23:47:30 +0700

    membuat prefix command

--- index.js
+++ index.js
@@ -10,6 +10,28 @@ async function startBot() {
     bot.onText(/\/start/, (msg) => {
         bot.sendMessage(msg.chat.id, "hellow brokk")
     })
+
+    bot.on('message', (msg) => {
+        const text = msg.text
+
+        const prefix = '/'
+        const isCmd = text.startsWith(prefix)
+        const cuy = text.slice(1).trim()
+        const args = cuy.split(" ")
+        const command = args.shift().toLowerCase()
+
+        if (!isCmd) return
+
+        switch (command) {
+            case 'tes':
+                bot.sendMessage(msg.chat.id, 'testing berhasil')
+                break;
+        
+            default:
+                break;
+        }
+    })
+
 }
 
 startBot()
